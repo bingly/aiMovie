ec622cdd41731c03f1d5fd3fc2307899325be80e
xmli2 <xmli2@admindeMac-mini.local>
2015-09-10 15:46:37 +0800

    Initial Commit

---
+++ AutoAdapt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoAdapt
+//
+//  Created by xmli2 on 15/9/10.
+//  Copyright (c) 2015年 iflytek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
