eca01ba07962d7a42a3f34e2b3514a34dfbed501
xmli2 <xmli2@admindeMac-mini.local>
2015-09-19 16:14:14 +0800

    creat git profile

--- AutoAdapt/ViewController.m
+++ AutoAdapt/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIWebView *webview;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *btnBack;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
+
+- (IBAction)backWebPage:(id)sender;
+
 @end
 
 @implementation ViewController
@@ -17,11 +23,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSURL *url = [NSURL URLWithString:@"http://61.132.255.99/h"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webview loadRequest:request];
+    _webview.scalesPageToFit = NO;
+    _webview.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)backWebPage:(id)sender{
+    if (_webview.canGoBack) {
+        [_webview goBack];
+    }
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView{
+    _webview.hidden = true;
+    [_activityIndicator startAnimating];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView{
+    _activityIndicator.hidden = true;
+    _webview.hidden = false;
 }
 
 @end
